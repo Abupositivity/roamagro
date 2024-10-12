facc8cfd901b8ea615e359c25f503243321864d8
abupositivity <abupositivity@gmail.com>
2024-10-12 15:29:55 +0100

    marketplace model update

--- backend/models/MarketplaceItem.js
+++ backend/models/MarketplaceItem.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const MarketplaceItemSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String },
-    category: { type: String, required: true },
-    price: { type: Number, required: true },
-    location: { type: String },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    createdAt: { type: Date, default: Date.now },
+  title: { type: String, required: true },
+  description: { type: String },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+  location: { type: String },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('MarketplaceItem', MarketplaceItemSchema);
