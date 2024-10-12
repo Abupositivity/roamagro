d2b04529f8b6c56e3b6ca8363e1aa0d545816055
abupositivity <abupositivity@gmail.com>
2024-10-12 14:15:33 +0100

    routes for marketplace item

---
+++ backend/routes/marketplaceRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { createMarketplaceItem, getMarketplaceItems } = require('../controllers/marketplaceController');
+const authMiddleware = require('../middlewares/authMiddleware');
+const routes = express.Router();
+
+router.use(authMiddleware);
+
+//routes for Marketplace Item
+router.post('/', createMarketplaceItem);
+router.get('/', getMarketplaceItems);
+
+module.exports = router;
