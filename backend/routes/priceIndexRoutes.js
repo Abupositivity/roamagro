c1ed583f0e2ea6732e558edc9156199724320a12
abupositivity <abupositivity@gmail.com>
2024-10-16 09:56:55 +0100

    routes for price index update

--- backend/routes/priceIndexRoutes.js
+++ backend/routes/priceIndexRoutes.js
@@ -1,4 +1,4 @@
-const express = ('express');
+const express = require('express');
 const { createPriceIndex, getPriceIndexes } = require('../controllers/priceIndexController');
 const router = express.Router();
 
