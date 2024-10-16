692dfe9eb6413fb690fbfc053248a462fd96c548
abupositivity <abupositivity@gmail.com>
2024-10-16 02:41:41 +0100

    update for authentication route to include google login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const passport = require('passport');
 const { registerUser, loginUser } = require('../controllers/authController');
 const router = express.Route();
 
 // Routes to rigister and login user
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+// Routes for google OAuth
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+        res.redirect('/dashboard');
+    }
+);
+
+router.get('/logout', (req, res) => {
+    req.logout((err) => {
+        if (err) return res.status(500).json({ error: 'Failed to logout' });
+        res.redirect('/');
+    });
+});
 
 module.exports = router;
