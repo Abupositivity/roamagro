0f88380efcde22bbef6f182a07ba1a39a1962c0c
abupositivity <abupositivity@gmail.com>
2024-10-17 09:55:38 +0100

    User's main Interface after logging in, providing access to all core functionalities.

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Container, Grid, Typography } from '@material-ui/core';
+import FarmProject from '../components/farmProjects/FarmProject';
+import Marketplace from '../components/marketplace/Marketplace';
+import PriceIndex from '../components/priceIndex/PriceIndex';
+import Community from '../components/community/Community';
+import ThemeSwitcher from '../components/ThemeSwitcher';
+import LanguageSwitcher from '../components/LanguageSwitcher';
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <Typography variant="h4">Dashboard</Typography>
+      <ThemeSwitcher />
+      <LanguageSwitcher />
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <FarmProject />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Marketplace />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <PriceIndex />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Community />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Dashboard;
