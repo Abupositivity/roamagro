b14b5c8c689553eb70261f1b97a551971784b261
abupositivity <abupositivity@gmail.com>
2024-10-17 10:20:16 +0100

    Frontend entry point

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/theme';
+import App from './App';
+import './i18n';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
