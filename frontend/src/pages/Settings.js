da8f4bfee602fa2c54017d865f03ba3b5633cab6
abupositivity <abupositivity@gmail.com>
2024-10-17 11:19:43 +0100

    App serttings page update to remove unused import switch

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, Switch, FormControlLabel } from '@material-ui/core';
+import { Container, Typography, FormControlLabel } from '@material-ui/core';
 import LanguageSwitcher from '../components/LanguageSwitcher';
 import ThemeSwitcher from '../components/ThemeSwitcher';
 
