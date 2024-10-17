4314a5b58fef52db131a8187353a54a1ecaa9a42
abupositivity <abupositivity@gmail.com>
2024-10-17 11:16:46 +0100

    Marketplace component update to remove updateListing import

--- frontend/src/components/marketplace/Marketplace.js
+++ frontend/src/components/marketplace/Marketplace.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchListings, createListing, updateListing } from '../../redux/actions/marketplaceActions';
+import { fetchListings, createListing } from '../../redux/actions/marketplaceActions';
 import { Container, Grid, TextField, Button, Typography, Paper, Select, MenuItem } from '@material-ui/core';
 
 const Marketplace = () => {
