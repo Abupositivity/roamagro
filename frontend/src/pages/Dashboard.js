948849c43d133c283866db206784bf479cfd58d9
abupositivity <abupositivity@gmail.com>
2024-10-24 12:11:55 +0100

    upgraded the dashboard to include features such as posting, agri feed etc.

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,32 +1,52 @@
-import React from 'react';
-import { Container, Grid, Typography } from '@mui/material';
-import FarmProject from '../components/farmProjects/FarmProject';
-import Marketplace from '../components/marketplace/Marketplace';
-import PriceIndex from '../components/priceIndex/PriceIndex';
-import Community from '../components/community/Community';
-import ThemeSwitcher from '../components/ThemeSwitcher';
-import LanguageSwitcher from '../components/LanguageSwitcher';
+import React, { useState } from 'react';
+import { IconButton, Typography, Container, Box, TextField } from '@mui/material';
+import AgriFeed from '../components/community/AgriFeed';
 
 const Dashboard = () => {
+  const [postContent, setPostContent] = useState('');
+
+  const handleCreatePost = () => {
+    if (postContent) {
+      console.log('New Agri-Feed post:', postContent);
+      setPostContent('');
+    }
+  };
+
   return (
-    <Container>
-      <Typography variant="h4">Dashboard</Typography>
-      <ThemeSwitcher />
-      <LanguageSwitcher />
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <FarmProject />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Marketplace />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <PriceIndex />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Community />
-        </Grid>
-      </Grid>
+    <Container maxWidth="lg">
+      {/* Space to Create a New Agri-Feed Post */}
+      <Box mt={4}>
+        <Typography variant="h6" gutterBottom>
+          Create New Post
+        </Typography>
+        <TextField
+          fullWidth
+          multiline
+          rows={3}
+          variant="outlined"
+          placeholder="Share tips or insights on agribusiness..."
+          value={postContent}
+          onChange={(e) => setPostContent(e.target.value)}
+        />
+        <Box mt={-5}>
+          <IconButton
+            variant="contained"
+            color="primary"
+            onClick={handleCreatePost}
+            style={{ marginTop: '50px' }}
+          >
+            Post
+          </IconButton>
+        </Box>
+      </Box>
+
+      {/* Agri-Feed Section with Scrolling */}
+      <Box mt={4} style={{ maxHeight: '400px', overflowY: 'auto' }}> {/* Set height and enable scrolling */}
+        <Typography variant="h5" gutterBottom>
+          Agri-Feed
+        </Typography>
+        <AgriFeed />
+      </Box>
     </Container>
   );
 };
