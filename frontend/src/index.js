6a99247d91529ac4e7aed0961b82185dba4eeeca
abupositivity <abupositivity@gmail.com>
2024-10-24 12:07:18 +0100

    Frontend index file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/theme';
 import App from './App';
@@ -7,9 +7,10 @@ import './i18n';
 
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
