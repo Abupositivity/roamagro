9976a1d445ca255566543ee13a7e838804d31ffe
abupositivity <abupositivity@gmail.com>
2024-10-24 12:15:00 +0100

    language switcher component update to ensure language is swiched properly across all pages

--- frontend/src/components/LanguageSwitcher.js
+++ frontend/src/components/LanguageSwitcher.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useEffect } from 'react';
 import { Button } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
 
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem('language') || 'en';
+    i18n.changeLanguage(savedLanguage);
+  }, [i18n]);
+
   const changeLanguage = (lang) => {
     i18n.changeLanguage(lang);
+    localStorage.setItem('language', lang); // Persist language
   };
 
   return (
