6a182fe1629bbe4dbd60a6541ec339c0f63d475e
abupositivity <abupositivity@gmail.com>
2024-10-24 12:05:30 +0100

    Multi-language update

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -4,16 +4,21 @@ import { initReactI18next } from 'react-i18next';
 const resources = {
   en: {
     translation: {
-      "welcome": "Welcome to RoamAgro",
+      "Welcome to RoamAgro!": "Welcome to RoamAgro",
       "login": "Login",
       "logout": "Logout",
     }
   },
   ha: {
     translation: {
-      "welcome": "Barka da zuwa RoamAgro",
+      "Welcome to RoamAgro": "Barka Da Zuwa RoamAgro",
+      "Your Agribusiness Companion!": "Abokin Sarrafa Harkokin Noma!",
+      "Login": "Shiga",
       "login": "Shiga",
-      "logout": "Fita",
+      "login with Google": "shiga ta Google",
+      "Login with Email": "Shiga ta Email",
+      "Register New User": "Yi Sabuwar Regista",
+      "Get Started": "Fara",
     }
   }
 };
@@ -25,7 +30,7 @@ i18n
     lng: "en", // Default language
     fallbackLng: "en",
     interpolation: {
-      escapeValue: false
+      escapeValue: true
     }
   });
 
