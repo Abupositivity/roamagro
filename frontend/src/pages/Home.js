14a132e9e38c1c087c8e8cb589c5cca2b23ca445
abupositivity <abupositivity@gmail.com>
2024-10-24 12:21:26 +0100

    Frontend Home Page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import { Container, Typography, Button } from '@mui/material';
+import { Container, Typography, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import './Home.css';
+import logo from '../assets/images/logo.svg';
 
 const Home = () => {
   const { t } = useTranslation();
 
   return (
-    <Container>
-      <Typography variant="h3">{t('Welcome to RoamAgro')}</Typography>
-      <Typography variant="h6">{t('Manage your farm projects, track prices, and more!')}</Typography>
-      <Button component={Link} to="/dashboard" color="primary" variant="contained">
-        {t('Get Started')}
+    <Container className="home-container" maxWidth="sm" sx={{ textAlign: 'center', mt: 4 }}>
+      {/* Logo Display */}
+      <Box sx={{ mb: 4 }}>
+        <img src={logo} alt="RoamAgro Logo" className="home-logo" />
+      </Box>
+
+      {/* Welcome Text */}
+      <Typography variant="h4" gutterBottom>
+        {t('Welcome to RoamAgro')}
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        {t('Your Agribusiness Companion!')}
+      </Typography>
+
+      {/* Email Login */}
+      <Button
+        component={Link}
+        to="/login"
+        color="primary"
+        variant="contained"
+        sx={{ mb: 2, width: '100%' }}
+      >
+        {t('Login')}
+      </Button>
+
+      {/* Register New User */}
+      <Button
+        component={Link}
+        to="/register"
+        color="primary"
+        variant="outlined"
+        sx={{ mb: 2, width: '100%' }}
+      >
+        {t('Register New User')}
       </Button>
     </Container>
   );
