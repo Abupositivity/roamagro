92a61e6f91d5d1a2b4d7a07cf8ab989f1eb118f6
abupositivity <abupositivity@gmail.com>
2024-10-28 09:36:04 +0100

    GoogleOAuthProvider update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import './styles/theme';
 import App from './App';
 import './i18n';
-
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 );
