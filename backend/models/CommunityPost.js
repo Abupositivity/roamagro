e995d41ca42a18edb4120e05aa5e29c709de85ea
abupositivity <abupositivity@gmail.com>
2026-07-15 18:03:44 +0100

    Added likes, reusable Comment schema, timestamps

--- backend/models/CommunityPost.js
+++ backend/models/CommunityPost.js
@@ -1,12 +1,54 @@
 const mongoose = require('mongoose');
 
-const CommunityPostSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    category: { type: String, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    comments: [{ user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, content: String }],
-    createdAt: { type: Date, default: Date.now }
+const CommentSchema = new mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    content: {
+        type: String,
+        required: true
+    }
+
+},
+{
+    timestamps: true
+});
+
+const CommunityPostSchema = new mongoose.Schema(
+{
+    title: {
+        type: String,
+        required: true
+    },
+
+    content: {
+        type: String,
+        required: true
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    comments: [CommentSchema],
+
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+
+},
+{
+    timestamps: true
 });
 
 module.exports = mongoose.model('CommunityPost', CommunityPostSchema);
